e53ab568a991a35e09b3d7fdcc2bc82a647c64ea
顾云翔 <guyunxiang32@gmail.com>
2024-03-18 17:06:24 -0400

    update comment

--- bin/start.js
+++ bin/start.js
@@ -1,22 +1,22 @@
 const { exec } = require("child_process");
 const path = require("path");
 
-// 启动frontend
+// start frontend
 const frontend = exec("npm start", {
   cwd: path.resolve(__dirname, "../frontend"),
 });
 
-// 启动backend
+// start backend
 const backend = exec("npm start", {
   cwd: path.resolve(__dirname, "../backend"),
 });
 
-// 监听frontend的输出
+// listening frontend output
 frontend.stdout.on("data", (data) => {
   console.log(`[frontend]: ${data}`);
 });
 
-// 监听backend的输出
+// listening backend output
 backend.stdout.on("data", (data) => {
   console.log(`[backend]: ${data}`);
 });
