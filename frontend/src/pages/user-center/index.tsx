7606af9183f215869ba01d84d23b3f9f858f645b
guyunxiang <guyunxiang32@gmail.com>
2024-06-29 18:54:34 -0400

    calculate chapter data in accuracy rate table

--- frontend/src/pages/user-center/index.tsx
+++ frontend/src/pages/user-center/index.tsx
@@ -8,14 +8,20 @@ interface Test {
   id: number;
   accuracyCount: number;
   accuracyRate: number;
+  totalCount: number;
   createdAt: Date;
 }
 
 interface MistakeData {
   chapterNo: number;
   testPaperNo: number;
+  sectionNo: number;
   vocabularyCount: number;
   tests: Test[]; // Specify that tests is an array of Test objects
+}
+
+interface ChapterGroup {
+  [key: number]: MistakeData[]
 }
 
 const UserCenter = () => {
@@ -33,7 +39,7 @@ const UserCenter = () => {
   const fetchDictationMistakes = async () => {
     const { success, data } = await get("/api/dictation/mistakes");
     if (success) {
-      setMistakesData(data);
+      setMistakesData(calculateChapterData(data));
       // Find the maximum number of tests for any mistake data
       data.forEach((item: MistakeData) => {
         if (item.tests.length > maxTestTimes) {
@@ -43,22 +49,78 @@ const UserCenter = () => {
     }
   }
 
+  // Calculate chapter data
+  const calculateChapterData = (data: MistakeData[]) => {
+    // group by chapter
+    const chapterGroups = data.reduce((acc: ChapterGroup, item) => {
+      if (!acc[item.chapterNo]) {
+        acc[item.chapterNo] = [];
+      }
+      acc[item.chapterNo].push(item);
+      return acc;
+    }, {});
+
+    // process each chapter
+    const result = [];
+    for (const [chapterNo, items] of Object.entries(chapterGroups)) {
+      // Calculate total vocabular in this chapter
+      const totalVocabularyCount = items.reduce((sum: number, item: MistakeData) => sum + item.vocabularyCount, 0);
+      // Get Maximum test times
+      const maxTestsLength = Math.max(...items.map((item: MistakeData) => item.tests.length));
+      // Calculate summary test array
+      const summaryTests = [];
+      for (let i = 0; i < maxTestsLength; i++) {
+        const testSummary = items.reduce((acc: Test, item: MistakeData) => {
+          if (item.tests[i]) {
+            acc.accuracyCount += item.tests[i].accuracyCount;
+            acc.totalCount += item.vocabularyCount;
+          }
+          return acc;
+        }, { accuracyCount: 0, totalCount: 0 });
+        // calculate accuracy rate
+        const accuracyRate = (testSummary.accuracyCount / testSummary.totalCount) * 100;
+        summaryTests.push({
+          accuracyCount: testSummary.accuracyCount,
+          accuracyRate: parseFloat(accuracyRate.toFixed(2)),
+          createdAt: items[items.length - 1].tests[i]?.createdAt || null
+        });
+      }
+
+      const chapterSummary = {
+        chapterNo: parseInt(chapterNo),
+        vocabularyCount: totalVocabularyCount,
+        tests: summaryTests
+      };
+
+      // insert data
+      const insertIndex = result.findIndex(item => item.chapterNo === parseInt(chapterNo));
+      if (insertIndex === -1) {
+        result.push(chapterSummary);
+        result.push(...items);
+      } else {
+        result.splice(insertIndex, 0, chapterSummary);
+      }
+    }
+
+    return result;
+  }
+
   // Function to render the <th> elements for table headers
   const renderTheadTimes = (isIndex: boolean) => {
     const thDoms: ReactElement[] = [];
     if (isIndex) {
       // Render index columns
       for (let i = 0; i < maxTestTimes; i++) {
-        thDoms.push(<th key={`index-${i}`} colSpan={3} className="border border-primary text-primary px-3">{i + 1}</th>);
+        thDoms.push(<th key={`index-${i}`} colSpan={3} className="border border-primary px-3">{i + 1}</th>);
       }
     } else {
       // Render check, rate, and date columns
       for (let i = 0; i < maxTestTimes; i++) {
         thDoms.push(
           <React.Fragment key={`fragment-${i}`}>
-            <th key={`check-${i}`} className="border border-primary text-primary px-3 min-w-16">✓</th>
-            <th key={`rate-${i}`} className="border border-primary text-primary px-3">Rate</th>
-            <th key={`date-${i}`} className="border border-primary text-primary px-3">Date</th>
+            <th key={`check-${i}`} className="border border-primary px-3 min-w-16">✓</th>
+            <th key={`rate-${i}`} className="border border-primary px-3">Rate</th>
+            <th key={`date-${i}`} className="border border-primary px-3">Date</th>
           </React.Fragment>
         );
       }
@@ -66,13 +128,27 @@ const UserCenter = () => {
     return thDoms;
   }
 
+  // render record title
+  const renderTitle = (chapterNo: number, testPaperNo: number, sectionNo: number) => {
+    if (testPaperNo) return "Test Paper" + testPaperNo;
+    if (sectionNo) return "Section " + sectionNo;
+    return "Chapter " + chapterNo;
+  }
+
+  const renderChapterClass = (testPaperNo: number, sectionNo: number) => {
+    if (testPaperNo || sectionNo) {
+      return "";
+    }
+    return "bg-secondary-200";
+  }
+
   return (
     <div className="container mx-auto mt-8 overflow-auto">
-      <table className="border-collapse border border-primary text-center" id="accuracy-rate-table">
-        <thead className="bg-[#fdba74]">
+      <table className="border-collapse border border-primary text-center text-[#92400e]" id="accuracy-rate-table">
+        <thead className="bg-secondary-500">
           <tr>
-            <th rowSpan={2} className="border border-primary text-primary px-3 whitespace-nowrap">Test Paper</th>
-            <th rowSpan={2} className="border border-primary text-primary px-3">Vocabulary</th>
+            <th rowSpan={2} className="border border-primary px-3 whitespace-nowrap">Test Paper</th>
+            <th rowSpan={2} className="border border-primary px-3">Vocabulary</th>
             {renderTheadTimes(true)}
           </tr>
           <tr>
@@ -81,27 +157,27 @@ const UserCenter = () => {
         </thead>
         <tbody>
           {
-            mistakesData.map(({ chapterNo, testPaperNo, vocabularyCount, tests }) => (
-              <tr key={testPaperNo}>
-                <td className="border border-primary">Test Paper {testPaperNo}</td>
+            mistakesData.map(({ chapterNo, testPaperNo, sectionNo, vocabularyCount, tests }) => (
+              <tr key={testPaperNo || sectionNo || chapterNo} className={renderChapterClass(testPaperNo, sectionNo)}>
+                <td className="border border-primary">{renderTitle(chapterNo, testPaperNo, sectionNo)}</td>
                 <td className="border border-primary">{vocabularyCount}</td>
                 {
                   tests.map((test: Test, idx: number) => (
                     <React.Fragment key={`${testPaperNo}-${idx}`}>
                       <td className="border border-primary px-3">
-                        <Link className="hover:text-primary" to={`/training/${test.id}`} state={{ id: test.id }}>
-                          {test.accuracyCount}
-                        </Link>
+                        {test.accuracyCount}
                       </td>
                       <td className="border border-primary px-3">
-                        <Link className="hover:text-primary" to={`/training/${test.id}`} state={{ id: test.id }}>
-                          {test.accuracyRate}%
-                        </Link>
+                        {
+                          test.id ? (
+                            <Link className="text-primary hover:text-secondary-500" to={`/training/${test.id}`} state={{ id: test.id }}>
+                              {test.accuracyRate.toFixed(2)}%
+                            </Link>
+                          ) : `${test.accuracyRate.toFixed(2)}%`
+                        }
                       </td>
                       <td className="border border-primary px-3">
-                        <Link className="hover:text-primary" to={`/training/${test.id}`} state={{ id: test.id }}>
-                          {new Date(test.createdAt).toLocaleDateString()}
-                        </Link>
+                        {new Date(test.createdAt).toLocaleDateString()}
                       </td>
                     </React.Fragment>
                   ))
