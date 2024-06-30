1fbd7a9c77ca84a65b61c2245ce9647de7cca43b
guyunxiang <guyunxiang32@gmail.com>
2024-06-29 22:27:45 -0400

    add paused button

--- frontend/src/pages/user-center/training.tsx
+++ frontend/src/pages/user-center/training.tsx
@@ -5,7 +5,7 @@ import copy from 'clipboard-copy';
 import { get, post } from '../../utils/fetch';
 import { toast } from 'react-toastify';
 
-let timer:NodeJS.Timeout;
+let timer:number;
 interface VocabularyData {
   chapterNo: number;
   testPaperNo: number;
@@ -21,83 +21,82 @@ interface Word {
   correct: boolean;
 }
 
-
 const VocabularyTraining = () => {
   const { state: { id } } = useLocation();
 
-  const [word, setWord] = useState('');
   const [vocabularyData, setVocabularyData] = useState<VocabularyData>({
     chapterNo: 0,
     testPaperNo: 0,
     sectionNo: 0,
     words: []
   });
+  const [word, setWord] = useState('');
   const [input, setInput] = useState<string>('');
   const [coloredWord, setColoredWord] = useState<JSX.Element[]>([]);
   const [correctCount, setCorrectCount] = useState<number>(0);
+  const [paused, setPaused] = useState(false);
+
+  const { words } = vocabularyData;
 
   useEffect(() => {
-    getDictationMistakeData();
-  }, []);
-
-  useEffect(() => {
-    updateColoredWord(word);
-  }, [input]);
-  
-  // auto play the word audio
-  useEffect(() => {
-    clearTimeout(timer);
-    wordSpeaking()
-    return () => {
-      clearTimeout(timer);
-      console.log('Timer cleared');
-    }
-  }, [word]);
-
-  const wordSpeaking = () => {
-    const voices = speechSynthesis.getVoices();
-    const selectedVoice = voices.find(voice => voice.name == 'Google US English');
-
-    const utterance = new SpeechSynthesisUtterance(word);
-    if (selectedVoice) {
-      utterance.voice = selectedVoice;
-    } else {
-      console.warn('Selected voice is undefined or not found.');
-      // Handle the case where selectedVoice is undefined
-      return;
-    }
-
-    speechSynthesis.speak(utterance);
-    timer = setTimeout(wordSpeaking, 3000);
-  }
-
-  // Get DictationMistake data via id;
-  const getDictationMistakeData = async () => {
-    await get('/api/dictation/mistake', { id }).then(({ success, data }) => {
-      if (success && data) {
-        // Find the latest practice word
-        const foundWord = data.words.find((word: Word) => (!word.correct && word.practiceCount <= 1));
-        setVocabularyData(data);
-        if (foundWord) {
-          const { word, practiceCount } = foundWord;
-          setWord(word);
-          setCorrectCount(practiceCount);
-          updateColoredWord(word);
+    // Get DictationMistake data via id;
+    const getDictationMistakeData = async () => {
+      await get('/api/dictation/mistake', { id }).then(({ success, data }) => {
+        if (success && data) {
+          // Find the latest practice word
+          const foundWord = data.words.find((word: Word) => (!word.correct && word.practiceCount <= 1));
+          setVocabularyData(data);
+          if (foundWord) {
+            const { word, practiceCount } = foundWord;
+            setWord(word);
+            setCorrectCount(practiceCount);
+          }
         }
-      }
-    });
-  }
+      });
+    }
+    // Get dictation mistake list
+    getDictationMistakeData();    
+  }, [id]);
 
-  const updateColoredWord = (word: string) => {
-    const newColoredWord = word.split('').map((letter: string, index: number) => {
-      let color = 'black';
-      if (index < input.length) {
-        color = input[index] === letter ? 'green' : 'red';
+  useEffect(() => {
+    // Update type status
+    const updateColoredWord = (word: string) => {
+      const newColoredWord = word.split('').map((letter: string, index: number) => {
+        let color = 'black';
+        if (index < input.length) {
+          color = input[index] === letter ? 'green' : 'red';
+        }
+        return <span key={index} style={{ color }} className="cursor-pointer">{letter}</span>;
+      });
+      setColoredWord(newColoredWord);
+    };
+    updateColoredWord(word);
+  }, [input, word]);
+  
+  useEffect(() => {
+    const wordSpeaking = () => {
+      clearTimeout(timer);
+      if (paused) return;
+
+      const voices = speechSynthesis.getVoices();
+      const selectedVoice = voices.find(voice => voice.name === 'Google US English');
+
+      const utterance = new SpeechSynthesisUtterance(word);
+      if (selectedVoice) {
+        utterance.voice = selectedVoice;
       }
-      return <span key={index} style={{ color }}>{letter}</span>;
-    });
-    setColoredWord(newColoredWord);
-  };
+
+      speechSynthesis.speak(utterance);
+      timer = setTimeout(wordSpeaking, 3000) as any as number;
+    }
+    wordSpeaking();
+    
+    return () => {
+      // Stop timer when leave this page
+      clearTimeout(timer);
+      // console.log('Timer cleared');
+    }
+  }, [word, paused]);
 
   // Update practice count to database
   const updatePracticeCount = async () => {
@@ -140,12 +139,12 @@ const VocabularyTraining = () => {
     updateLocalVocabularyData();
     setWord(nextWord);
     setInput('');
-    updateColoredWord(nextWord);
     // Get practice count from new word
     const practiceCount = vocabularyData.words.find(({ word }) => word === nextWord)?.practiceCount ?? 0;
     setCorrectCount(practiceCount);
   }
 
+  // Switch next word to training
   const handleWordListClick = async (event: React.MouseEvent<HTMLUListElement>) => {
     const target = event.target as HTMLElement;
     if (target.tagName.toLowerCase() === 'li') {
@@ -156,6 +155,12 @@ const VocabularyTraining = () => {
     }
   };
 
+  // On paused audio
+  const handlePauseAudio = () => {
+    setPaused(!paused);
+  }
+
+  // On input word
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
@@ -170,6 +175,7 @@ const VocabularyTraining = () => {
     }
   };
 
+  // on Press Tab key
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Tab') {
       e.preventDefault();
@@ -185,12 +191,11 @@ const VocabularyTraining = () => {
     }
   }
 
+  // copy the word
   const handleCopyWord = () => {
     copy(word);
     toast.success("Copied to clipboard.");
   }
-
-  const { words } = vocabularyData;
 
   const RenderVocabularyList = () => {
     const correctClass = "text-gray-400 border-gray-400 font-normal";
@@ -224,8 +229,8 @@ const VocabularyTraining = () => {
 
   return (
     <div className='mt-4 text-center flex flex-1 flex-col'>
-      <div className="inline-block">
-        <h1 className="text-9xl font-black font-sans mb-6 relative cursor-pointer" onClick={handleCopyWord}>
+      <div className="inline-block relative">
+        <h1 className="text-9xl font-black font-sans mb-6 relative" onClick={handleCopyWord}>
           {coloredWord}
           {
             correctCount > 0 && (
@@ -235,6 +240,9 @@ const VocabularyTraining = () => {
             )
           }
         </h1>
+        <button className='absolute top-0 right-0 mr-4 px-3 text-primary border rounded border-primary' onClick={handlePauseAudio}>
+          { paused ? "Play" : "Pause" }
+        </button>
       </div>
       <div className="container mx-auto flex-1 justify-center gap-8 overflow-auto mb-10">
         <RenderVocabularyList />
