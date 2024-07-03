8976a50a450f2dc86d49c038302c4f0b03629f18
Yunxiang Gu <guyunxiang32@gmail.com>
2024-07-03 09:19:41 -0400

    optimize code for create misspelled table

--- backend/controllers/test.js
+++ backend/controllers/test.js
@@ -39,7 +39,7 @@ exports.savePaperTest = async (req, res) => {
     });
 
     // to do create a misspelled words record
-    await createMisspelledWords(chapter, paper, words, userId, _id);
+    await createMisspelledWords(_id);
 
   } catch (error) {
     console.log(error);
@@ -50,41 +50,84 @@ exports.savePaperTest = async (req, res) => {
   }
 }
 
-// create misspelled words table
-const createMisspelledWords = async (chapter, paper, testWords, userId, testId) => {
-  try {
-    // Fetch the vocabulary list for this chapter and paper
-    const vocabularyList = await VocabularyList.findOne({ chapterNo: chapter, testPaperNo: paper });
+const calculateMisspelledData = (testWords, vocabularyList) => {
+  // Compare test words with vocabulary list
+  const whitelist = [
+    { original: 'centre', alternative: 'center' },
+    { original: 'booklist', alternative: 'book list' }
+  ];
 
+  // Compare test words with vocabulary list
+  const misspelledWords = vocabularyList.words.filter(vocabWord =>
+    !testWords.some(testWord => {
+      const normalizedVocabWord = vocabWord.word.toLowerCase();
+      const normalizedTestWord = testWord.toLowerCase();
+
+      // Check if words are equal without considering the whitelist
+      if (normalizedVocabWord === normalizedTestWord) {
+        return false; // Exact match, not misspelled
+      }
+
+      // Check against whitelist
+      for (const pair of whitelist) {
+        if ((normalizedVocabWord === pair.original && normalizedTestWord === pair.alternative) ||
+          (normalizedVocabWord === pair.alternative && normalizedTestWord === pair.original)) {
+          return false; // Match found in whitelist, not misspelled
+        }
+      }
+
+      return true; // Not found in whitelist, consider as misspelled
+    })
+  );
+
+  // Calculate accuracy rate
+  const accuracyCount = (vocabularyList.words.length - misspelledWords.length);
+  const accuracyRate = (accuracyCount / vocabularyList.words.length) * 100;
+
+  return {
+    accuracyRate: accuracyRate,
+    accuracyCount: accuracyCount,
+    words: misspelledWords.map(word => ({
+      word: word.word,
+      practiceCount: 1
+    })),
+  }
+}
+
+// create misspelled words table
+const createMisspelledWords = async (testId, misspelledRecordId) => {
+  try {
+    const testRecord = await Test.findById(testId);
+    if (!testRecord) {
+      throw new Error('Vocabulary list not found');
+    }
+    const { chapterNo, paperNo, words, userId } = testRecord;
+
+    // Fetch the vocabulary list for this chapter and paper
+    const vocabularyList = await VocabularyList.findOne({ chapterNo: chapterNo, testPaperNo: paperNo });
     if (!vocabularyList) {
       throw new Error('Vocabulary list not found');
     }
 
-    // Compare test words with vocabulary list
-    const misspelledWords = vocabularyList.words.filter(vocabWord =>
-      !testWords.some(testWord => testWord.toLowerCase() === vocabWord.word.toLowerCase())
-    );
-
-    // Calculate accuracy rate
-    const accuracyCount = (vocabularyList.words.length - misspelledWords.length);
-    const accuracyRate = (accuracyCount / vocabularyList.words.length) * 100;
-
-    // Create new DictationMistake record
-    const newMistake = new DictationMistake({
-      chapterNo: chapter,
-      testPaperNo: paper,
-      accuracyRate: accuracyRate,
-      accuracyCount: accuracyCount,
-      words: misspelledWords.map(word => ({
-        word: word.word,
-        practiceCount: 1
-      })),
-      userId,
-      testId,
-    });
-
+    let newMistake;
+    const misspelledRecord = calculateMisspelledData(words, vocabularyList);
+    if (!misspelledRecordId) {
+      // Create new DictationMistake record
+      newMistake = new DictationMistake({
+        chapterNo: chapterNo,
+        testPaperNo: paperNo,
+        ...misspelledRecord,
+        userId,
+        testId,
+      });
+    } else {
+      newMistake = await DictationMistake.findByIdAndUpdate(
+        misspelledRecordId,
+        misspelledRecord,
+        { new: true }
+      );
+    }
     await newMistake.save();
-
     console.log('Misspelled words record created successfully');
   } catch (error) {
     console.error('Error creating misspelled words record:', error);
