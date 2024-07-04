9011e063d58559d04267304b81c511faaf7b879e
guyunxiang <guyunxiang32@gmail.com>
2024-07-03 22:24:17 -0400

    update config

--- frontend/src/utils/const.ts
+++ frontend/src/utils/const.ts
@@ -2,9 +2,28 @@ interface TestPaper {
   [key: string]: number
 }
 
+interface Chapter11Parts {
+  [key: string]: number[]
+}
+
 export const TEST_PAPERS: TestPaper = {
+  chapter2: 5,
   chapter3: 9,
   chapter4: 4,
   chapter5: 12,
+  chapter6: 18,
+  chapter7: 3,
+  chapter8: 16,
+  chapter9: 1,
+  chapter10: 1,
   chapter11: 4,
+  chapter12: 3
+}
+
+// define chapter 11, part1 & part2 word count
+export const CHAPTER11_PARTS: Chapter11Parts = {
+  section1: [145, 222],
+  section2: [129, 267],
+  section3: [139, 210],
+  section4: [550, 218]
 }
