b0c1bc66669f29d86a10e286da145ebd6baeb33d
Yunxiang Gu <guyunxiang32@gmail.com>
2024-09-02 20:35:53 -0400

    code format

--- backend/controllers/test.js
+++ backend/controllers/test.js
@@ -1,30 +1,25 @@
-const Test = require('../models/test');
+const Test = require("../models/test");
 const VocabularyList = require("../models/vocabularyList");
 const DictationMistake = require("../models/dictationMistake");
-const Whitelist = require('../models/whitelist');
+const Whitelist = require("../models/whitelist");
 
 const testController = require("./test");
 
 // save paper test
 exports.savePaperTest = async (req, res) => {
   try {
-
     const {
-      session: { userId }
+      session: { userId },
     } = req;
 
     if (!userId) {
       return res.status(401).json({
         success: false,
-        message: "Please log in first!"
+        message: "Please log in first!",
       });
     }
 
-    const {
-      chapter,
-      paper,
-      words,
-    } = req.body;
+    const { chapter, paper, words } = req.body;
 
     const newTest = new Test({
       chapterNo: chapter,
@@ -41,7 +36,7 @@ exports.savePaperTest = async (req, res) => {
 
     res.status(201).json({
       success: true,
-      message: "Submit successfully!"
+      message: "Submit successfully!",
     });
   } catch (error) {
     console.log(error);
@@ -50,21 +45,35 @@ exports.savePaperTest = async (req, res) => {
       message: error,
     });
   }
-}
+};
 
-const calculateMisspelledData = async (testWords, vocabularyList, originalWords) => {
-
+const calculateMisspelledData = async (
+  testWords,
+  vocabularyList,
+  originalWords
+) => {
   // TODO, version 18
-  const { whitelist } = await Whitelist.findOne({ version: 18 }).select("whitelist") ?? { whitelist: WHITELIST };
+  const { whitelist } = (await Whitelist.findOne({ version: 18 }).select(
+    "whitelist"
+  )) ?? { whitelist: WHITELIST };
 
   // ignore plural
   const ignorePlural = (word) => {
     const lowercaseWord = word.toLowerCase();
-    if (lowercaseWord.endsWith('ies')) {
-      return lowercaseWord.slice(0, -3) + 'y';
-    } else if (lowercaseWord.endsWith('ses') || lowercaseWord.endsWith('xes') || lowercaseWord.endsWith('ches') || lowercaseWord.endsWith('shes')) {
+    if (lowercaseWord.endsWith("ies")) {
+      return lowercaseWord.slice(0, -3) + "y";
+    } else if (
+      lowercaseWord.endsWith("ses") ||
+      lowercaseWord.endsWith("xes") ||
+      lowercaseWord.endsWith("ches") ||
+      lowercaseWord.endsWith("shes")
+    ) {
       return lowercaseWord.slice(0, -2);
-    } else if (lowercaseWord.endsWith('s') && lowercaseWord.length > 3 && !lowercaseWord.endsWith('ss')) {
+    } else if (
+      lowercaseWord.endsWith("s") &&
+      lowercaseWord.length > 3 &&
+      !lowercaseWord.endsWith("ss")
+    ) {
       return lowercaseWord.slice(0, -1);
     }
     return lowercaseWord;
@@ -74,10 +83,10 @@ const calculateMisspelledData = async (testWords, vocabularyList, originalWords)
     return ignorePlural(word1) === ignorePlural(word2);
   };
 
-  const misspelledWords = vocabularyList.words.filter(vocabWord => {
+  const misspelledWords = vocabularyList.words.filter((vocabWord) => {
     const normalizedVocabWord = vocabWord.word.toLowerCase();
 
-    return !testWords.some(testWord => {
+    return !testWords.some((testWord) => {
       const normalizedTestWord = testWord.toLowerCase();
 
       // Check for exact match
@@ -86,11 +95,14 @@ const calculateMisspelledData = async (testWords, vocabularyList, originalWords)
       }
 
       // Check against whitelist
-      return whitelist.some(pair =>
-        (wordsMatch(normalizedTestWord, pair.original) &&
-          pair.alternative.some(alt => wordsMatch(alt, normalizedVocabWord))) ||
-        (wordsMatch(normalizedVocabWord, pair.original) &&
-          pair.alternative.some(alt => wordsMatch(alt, normalizedTestWord)))
+      return whitelist.some(
+        (pair) =>
+          (wordsMatch(normalizedTestWord, pair.original) &&
+            pair.alternative.some((alt) =>
+              wordsMatch(alt, normalizedVocabWord)
+            )) ||
+          (wordsMatch(normalizedVocabWord, pair.original) &&
+            pair.alternative.some((alt) => wordsMatch(alt, normalizedTestWord)))
       );
     });
   });
@@ -99,49 +111,53 @@ const calculateMisspelledData = async (testWords, vocabularyList, originalWords)
   const accuracyCount = testWords.length - misspelledWords.length;
   const accuracyRate = (accuracyCount / testWords.length) * 100;
 
-  // Get original practice count for each misspelled word
-  const getOriginalPracticeCount = (word) => {
-    try {
-      if (!originalWords || originalWords.length === 0) {
-        return 0;
-      }
-      const { practiceCount } = originalWords.find(originalWord => wordsMatch(originalWord.word, word));
-      return practiceCount || 0;
-    } catch (error) {
-      console.error('Error getting original practice count:', error);
-    }
+  let practiceCount = 0;
+  let trainingDuration = 0;
+  if (originalWords) {
+    (practiceCount = originalWords[i].practiceCount),
+      (trainingDuration = originalWords[i].trainingDuration);
   }
 
   return {
     accuracyRate: accuracyRate.toFixed(2),
     accuracyCount: accuracyCount,
-    words: misspelledWords.map(word => ({
+    words: misspelledWords.map((word, i) => ({
       word: word.word,
-      practiceCount: getOriginalPracticeCount(word.word),
+      practiceCount,
+      trainingDuration,
     })),
-  }
-}
+  };
+};
 
 // create misspelled words table
 exports.createMisspelledWords = async (testId, misspelledRecordId) => {
   try {
     const testRecord = await Test.findById(testId);
     if (!testRecord) {
-      throw new Error('Vocabulary list not found');
+      throw new Error("Vocabulary list not found");
     }
     const { chapterNo, paperNo, words, userId } = testRecord;
 
     // Fetch the vocabulary list for this chapter and paper
-    const vocabularyList = await VocabularyList.findOne({ chapterNo: chapterNo, testPaperNo: paperNo });
+    const vocabularyList = await VocabularyList.findOne({
+      chapterNo: chapterNo,
+      testPaperNo: paperNo,
+    });
     if (!vocabularyList) {
-      throw new Error('Vocabulary list not found');
+      throw new Error("Vocabulary list not found");
     }
 
     // Fetch original misspelled record
-    const originalMisspelledRecord = await DictationMistake.findById(misspelledRecordId);
+    const originalMisspelledRecord = await DictationMistake.findById(
+      misspelledRecordId
+    );
 
     let newMistake;
-    const misspelledRecord = await calculateMisspelledData(words, vocabularyList, originalMisspelledRecord?.words);
+    const misspelledRecord = await calculateMisspelledData(
+      words,
+      vocabularyList,
+      originalMisspelledRecord?.words
+    );
     if (!misspelledRecordId) {
       // Create new DictationMistake record
       newMistake = new DictationMistake({
@@ -159,9 +175,9 @@ exports.createMisspelledWords = async (testId, misspelledRecordId) => {
         { new: true }
       );
     }
-    console.log('Misspelled words record created successfully');
+    console.log("Misspelled words record created successfully");
   } catch (error) {
-    console.error('Error creating misspelled words record:', error);
+    console.error("Error creating misspelled words record:", error);
     throw error;
   }
-}
+};
